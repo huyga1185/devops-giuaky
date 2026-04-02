89af57777f070b63bdd74554fbc972631eaa1637
huy <huylay1054@gmail.com>
2026-04-02 13:39:37 +0700

    chore: init database

---
+++ backend/db.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS sinhvien;
+CREATE DATABASE sinhvien;
+
+USE sinhvien;
+
+CREATE TABLE tt_sinhvien (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  hoten VARCHAR(512) NOT NULL,
+  mssv VARCHAR(10) NOT NULL UNIQUE,
+  lop VARCHAR(10) NOT NULL,
+  role VARCHAR(10) NOT NULL DEFAULT 'SV'
+);
+
+CREATE TABLE tai_khoan (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  mssv VARCHAR(10) NOT NULL UNIQUE,
+  hashed_password VARCHAR(255) NOT NULL,
+  CONSTRAINT tai_khoan_mssv_tt_sinhvien_mssv FOREIGN KEY (mssv) REFERENCES tt_sinhvien(mssv)
+);
+
+CREATE TABLE phien (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  phien_key VARCHAR(512) NOT NULL,
+  tk_id INT NOT NULL,
+  CONSTRAINT phien_tk_id_tai_khoan_id FOREIGN KEY (tk_id) REFERENCES tai_khoan(id)
+)
