4be23dbdf50cf12f8b451d2dcaacc05517192f3a
huy <huylay1054@gmail.com>
2026-04-02 14:28:16 +0700

    feat: check api health

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/api/health', (req, res) => {
+  res.status(200).json({status:"ok"});
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
